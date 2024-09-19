529dafe6e07321dcdd32e4299e42c531acc8f21c
Dan <monreal007@gmail.com>
2024-09-19 14:56:12 +0400

    Домашнее задание 5

---
+++ Exeptions/User.cs
@@ -0,0 +1,29 @@
+﻿namespace Exceptions
+{
+  /// <summary>
+  /// Пользователь
+  /// </summary>
+  public class User
+  {
+    /// <summary>
+    /// Идентификатор пользователя
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Имя пользователя.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Почта пользователя.
+    /// </summary>
+    public string Email { get; set; }
+
+    public User(string name, string email)
+    {
+      this.Name = name;
+      this.Email = email;
+    }
+  }
+}
